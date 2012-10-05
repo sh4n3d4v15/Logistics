2b95fcb8be22d82625ce92a7547d625c8e6514fd
unknown <daviess@JRH555J.chep.com>
2012-10-05 15:48:47 +0100

    package

--- app.js
+++ app.js
@@ -390,6 +390,5 @@ app.get('/fetch', function(req , res){
 // });
 
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+var port = process.env.PORT ||3000
+app.listen(port);
